6bc3f37f6af59d3e6d5975c581ce8f70ae05cad2
Muhammad Usama <lifeh2o@gmail.com>
2024-08-28 13:25:49 +0100

    some updates

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,9 @@
-const video = document.querySelector('video');
-let originalCanvas;
-let invertedCanvas;
-let originalCtx;
-let invertedCtx;
+let originalCanvas = document.createElement('canvas');
+let invertedCanvas = document.createElement('canvas');
+let originalCtx = originalCanvas.getContext('2d');
+let invertedCtx = invertedCanvas.getContext('2d');
+originalCanvas.id = 'originalCanvas';
+invertedCanvas.id = 'invertedCanvas';
 
 let maxFrames = 200;
 let framesBuffer = new Array(maxFrames);
@@ -11,71 +12,65 @@ let currentFrame = 0;
 let lastPlayedTime = null;
 let opacity = 0.5; // initial value
 
+const video = document.querySelector('video');
 if (video) {
-  originalCanvas = document.createElement('canvas');
-  invertedCanvas = document.createElement('canvas');
+    document.body.appendChild(originalCanvas);
+    let videoContainer = document.querySelector("#movie_player > div.html5-video-container"); // youtube
+    videoContainer.appendChild(invertedCanvas);
 
-  originalCanvas.id = 'originalCanvas';
-  invertedCanvas.id = 'invertedCanvas';
+    originalCanvas.width = video.videoWidth;
+    originalCanvas.height = video.videoHeight;
+    invertedCanvas.width = video.videoWidth;
+    invertedCanvas.height = video.videoHeight;
 
-  document.body.appendChild(originalCanvas);
-  let videoContainer = document.querySelector("#movie_player > div.html5-video-container");
-  videoContainer.appendChild(invertedCanvas);
-  
-  originalCtx = originalCanvas.getContext('2d');
-  invertedCtx = invertedCanvas.getContext('2d');
-  
-  originalCanvas.width = video.videoWidth;
-  originalCanvas.height = video.videoHeight;
-  invertedCanvas.width = video.videoWidth;
-  invertedCanvas.height = video.videoHeight;
-
-  originalCanvas.style.display = "none";
-  
-  invertedCanvas.style.opacity = opacity;
-  invertedCtx.globalCompositeOperation = "lighter";
-    
-  function draw() {
-    // skip duplicate frames
-    let currentTime = video.currentTime;
-    if (currentTime === lastPlayedTime) {
-      requestAnimationFrame(draw);
-      return;
-    }
-    lastPlayedTime = currentTime;
-
-    // Draw video to original canvas
-    originalCtx.drawImage(video, 0, 0, originalCanvas.width, originalCanvas.height);
-    originalCtx.wi
-
-    // Get frame data from original canvas and invert colors
-    let imageData = originalCtx.getImageData(0, 0, originalCanvas.width, originalCanvas.height);
-    let data = imageData.data;
-
-    // Invert color of each pixel
-    for (let i = 0; i < data.length; i += 4) {
-        data[i] = 255 - data[i]; // red
-        data[i + 1] = 255 - data[i + 1]; // green
-        data[i + 2] = 255 - data[i + 2]; // blue
-    }
-    framesBuffer[currentFrame] = imageData;
-    currentFrame = (currentFrame + 1) % maxFrames;
-
-    let delayedFramee = framesBuffer[Math.abs((currentFrame + maxFrames - frameDelay) % maxFrames)];
-    if (delayedFramee) {
-        invertedCtx.putImageData(delayedFramee, 0, 0);
-    }
-
-    // Apply opacity value
+    originalCanvas.style.display = "none";
     invertedCanvas.style.opacity = opacity;
 
-    requestAnimationFrame(draw);
-  }
-  draw();
+    invertedCtx.globalCompositeOperation = "lighter";
+
+    function draw() {
+        // skip duplicate frames
+        let currentTime = video.currentTime;
+        if (currentTime === lastPlayedTime) {
+            requestAnimationFrame(draw);
+            return;
+        }
+        lastPlayedTime = currentTime;
+        ///////////////////////////////////////
+
+        // Draw video to original canvas
+        originalCtx.drawImage(video, 0, 0, originalCanvas.width, originalCanvas.height);
+        //originalCtx.wi
+
+        // Get frame data from original canvas and invert colors
+        let imageData = originalCtx.getImageData(0, 0, originalCanvas.width, originalCanvas.height);
+        let data = imageData.data;
+
+        // Invert color of each pixel
+        for (let i = 0; i < data.length; i += 4) {
+            data[i] = 255 - data[i]; // red
+            data[i + 1] = 255 - data[i + 1]; // green
+            data[i + 2] = 255 - data[i + 2]; // blue
+        }
+        framesBuffer[currentFrame] = imageData;
+        currentFrame = (currentFrame + 1) % maxFrames;
+
+        let delayedFramee = framesBuffer[Math.abs((currentFrame + maxFrames - frameDelay) % maxFrames)];
+        if (delayedFramee) {
+            invertedCtx.putImageData(delayedFramee, 0, 0);
+        }
+
+        // Apply opacity value
+        invertedCanvas.style.opacity = opacity;
+
+        requestAnimationFrame(draw);
+    }
+
+    draw();
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if(request.action === "start") {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "start") {
         frameDelay = Number(request.frameDelay);
         opacity = Number(request.opacity);
         sendResponse({status: "Started"});
